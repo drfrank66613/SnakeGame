adb37f8a9313ce01e049ee02ed9a232cb9ef7e82
George Kennedy <dm66613@gmail.com>
2020-10-07 02:05:15 +0800

    Added scores to the game, Fixed snake movement speed and made the food spawn in a new location if it has been eaten

--- SnakeGame/Program.cs
+++ SnakeGame/Program.cs
@@ -18,16 +18,16 @@ namespace SnakeGame
 
             // location info & display
             int x = 0, y = 2; // y is 2 to allow the top row for directions & space
-            int dx = 1, dy = 0;
-            int consoleWidthLimit = Console.WindowWidth;
-            int consoleHeightLimit = Console.WindowHeight;
+            int dx = 2, dy = 0;
+            int consoleWidthLimit = Console.WindowWidth; // 120
+            int consoleHeightLimit = Console.WindowHeight; // 30
 
             // clear to color
             Console.BackgroundColor = ConsoleColor.DarkGray;
             Console.Clear();
 
             // delay to slow down the character movement so you can see it
-            int delayInMillisecs = 50;
+            int delayInMillisecs = 70;
 
             // whether to keep trails
             bool trail = false;
@@ -36,7 +36,10 @@ namespace SnakeGame
             string obstacle = "||";
             string food = "F";
 
+            // The current score based on the food eaten
+            int currentScore = 0;
 
+            
             // Generating random number for the amount of obstacles (create a number between 1 and 3)
             Random rnd = new Random();
             int numOfObstacles = rnd.Next(1, 4);
@@ -45,32 +48,35 @@ namespace SnakeGame
             // 2D Array to stores the x and y position of the obstacles
             int[,] obstaclePositions = new int[3, 2];
 
-
+            
             // Generating the obstacles with random positions 
             for (int i = 0; i < numOfObstacles; i++)
             {
-                obstaclePositions[i, 0] = rnd.Next(5, 110);
-                obstaclePositions[i, 1] = rnd.Next(5, 30);
+                obstaclePositions[i, 0] = rnd.Next(5/2, 110/2)*2;
+                obstaclePositions[i, 1] = rnd.Next(5/2, 30/2)*2;
 
                 Console.SetCursorPosition(obstaclePositions[i, 0], obstaclePositions[i, 1]);
                 Console.Write(obstacle);
+               
             }
 
             //Generates food on random location
-            int foodX = rnd.Next(5, 115);
-            int foodY = rnd.Next(5, 29);
+            int foodX = rnd.Next(5/2, 115/2)*2;
+            int foodY = rnd.Next(5/2, 29/2)*2;
 
             Console.SetCursorPosition(foodX, foodY);
             
 
             if (foodX == obstaclePositions[0, 0] && foodY == obstaclePositions[0, 1] || foodX == obstaclePositions[1, 0] && foodY == obstaclePositions[1, 1] || foodX == obstaclePositions[2, 0] && foodY == obstaclePositions[2, 1])
             {
-                foodX = rnd.Next(5, 115);
-                foodY = rnd.Next(5, 115);
+                foodX = rnd.Next(5/2, 115/2)*2;
+                foodY = rnd.Next(5/2, 115/2)*2;
                 Console.SetCursorPosition(foodX, foodY);
             }
             Console.Write(food);
 
+
+            
             do // until escape
             {
                 // print directions at top, then restore position
@@ -78,7 +84,8 @@ namespace SnakeGame
                 ConsoleColor cc = Console.ForegroundColor;
                 Console.ForegroundColor = ConsoleColor.Black;
                 Console.SetCursorPosition(0, 0);
-                Console.WriteLine("Arrows move up/down/right/left. Press 'esc' quit.");
+                Console.Write("Arrows move up/down/right/left. Press 'esc' quit.");
+                Console.WriteLine("                                     Score : " + currentScore);
                 Console.SetCursorPosition(x, y);
                 Console.ForegroundColor = cc;
 
@@ -93,22 +100,22 @@ namespace SnakeGame
                         case ConsoleKey.UpArrow: //UP
                             dx = 0;
                             dy = -1;
-                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.ForegroundColor = ConsoleColor.Red;                            
                             break;
                         case ConsoleKey.DownArrow: // DOWN
                             dx = 0;
                             dy = 1;
-                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            Console.ForegroundColor = ConsoleColor.Cyan;                          
                             break;
                         case ConsoleKey.LeftArrow: //LEFT
-                            dx = -1;
+                            dx = -2; // Changed to -2 so that the snake movement speed looks similar when moving vertically and horizontally
                             dy = 0;
-                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.ForegroundColor = ConsoleColor.Green;                            
                             break;
                         case ConsoleKey.RightArrow: //RIGHT
-                            dx = 1;
+                            dx = 2; // Changed to 2 so that the snake movement speed looks similar when moving vertically and horizontally
                             dy = 0;
-                            Console.ForegroundColor = ConsoleColor.Black;
+                            Console.ForegroundColor = ConsoleColor.Black;       
                             break;
                         case ConsoleKey.Escape: //END
                             gameLive = false;
@@ -119,7 +126,14 @@ namespace SnakeGame
                 // find the current position in the console grid & erase the character there if don't want to see the trail
                 Console.SetCursorPosition(x, y);
                 if (trail == false)
+                {
                     Console.Write(' ');
+                    if (x == obstaclePositions[0, 0] && y == obstaclePositions[0, 1] || x == obstaclePositions[1, 0] && y == obstaclePositions[1, 1] || x == obstaclePositions[2, 0] && y == obstaclePositions[2, 1])
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                    
 
                 // calculate the new position
                 // note x set to 0 because we use the whole width, but y set to 1 because we use top row for instructions
@@ -135,11 +149,32 @@ namespace SnakeGame
                 if (y < 2)
                     y = consoleHeightLimit;
 
-
+                
                 // write the character in the new position
                 Console.SetCursorPosition(x, y);
                 Console.Write(ch);
 
+                // Increase the current score and spawn a new food location if the previous food has been eaten
+                if (x == foodX && y == foodY)
+                {
+                    currentScore += 10;
+
+                    foodX = rnd.Next(5 / 2, 115 / 2) * 2;
+                    foodY = rnd.Next(5 / 2, 29 / 2) * 2;
+
+                    Console.SetCursorPosition(foodX, foodY);
+
+
+                    if (foodX == obstaclePositions[0, 0] && foodY == obstaclePositions[0, 1] || foodX == obstaclePositions[1, 0] && foodY == obstaclePositions[1, 1] || foodX == obstaclePositions[2, 0] && foodY == obstaclePositions[2, 1])
+                    {
+                        foodX = rnd.Next(5 / 2, 115 / 2) * 2;
+                        foodY = rnd.Next(5 / 2, 115 / 2) * 2;
+                        Console.SetCursorPosition(foodX, foodY);
+                    }
+                    Console.Write(food);
+                }
+
+                
                 // pause to allow eyeballs to keep up
                 System.Threading.Thread.Sleep(delayInMillisecs);
 
